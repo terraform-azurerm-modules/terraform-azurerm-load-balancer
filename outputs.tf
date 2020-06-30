a012df1917e743badbad0d2f1aba65014835ecb5
Richard Cheney <richeney@microsoft.com>
2020-06-30 07:24:15 +0100

    Output load balancer backend address pool object

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,12 @@
-/*
-output "availability_set_id" {
-  value       = azurerm_availability_set.lb.id
-}
-*/
-
 output "load_balancer_private_ip_address" {
   value = azurerm_lb.lb.frontend_ip_configuration[0].private_ip_address
 }
 
-output "load_balancer_backend_address_pool_id" {
-  value = azurerm_lb_backend_address_pool.lb.id
+output "load_balancer_backend_address_pool" {
+  value = {
+    name = azurerm_lb_backend_address_pool.lb.name
+    id   = azurerm_lb_backend_address_pool.lb.id
+  }
 }
 
 /*
